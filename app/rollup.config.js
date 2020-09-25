905626a4cebf3449c0dca5cfd29d22c3f038f093
Florian Georg <florian.alexander.georg@gmail.com>
2020-09-25 13:35:51 +0200

    initial commit

---
+++ app/rollup.config.js
@@ -0,0 +1,75 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+import replace from '@rollup/plugin-replace';
+
+const production = !process.env.ROLLUP_WATCH;
+const api = 'http://localhost:7071/api';
+const API = process.env.API || production ? '/api' : api;
+
+export default {
+  input: 'public/main.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'public/build/bundle.js',
+  },
+  plugins: [
+    replace({
+      // 2 level deep object should be stringify
+      process: JSON.stringify({
+        env: {
+          isProd: production,
+          SVELTE_APP_API: API,
+        },
+      }),
+    }),
+    postcss(),
+
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration -
+    // consult the documentation for details:
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    resolve({
+      browser: true,
+      dedupe: ['svelte'],
+    }),
+    commonjs(),
+
+    // In dev mode, call `npm run start` once
+    // the bundle has been generated
+    !production && serve(),
+
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload('public'),
+
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser(),
+  ],
+  watch: {
+    clearScreen: false,
+  },
+};
+
+function serve() {
+  let started = false;
+
+  return {
+    writeBundle() {
+      if (!started) {
+        started = true;
+
+        require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+          stdio: ['ignore', 'inherit', 'inherit'],
+          shell: true,
+        });
+      }
+    },
+  };
+}
